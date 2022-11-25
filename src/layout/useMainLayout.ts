cd0bb6090ed5d3a7f5436d846f758b7c7f8b3bce
DeEKol <dekolodkin@gmail.com>
2022-11-25 12:05:32 +0500

    update: scroll routing

--- src/layout/useMainLayout.ts
+++ src/layout/useMainLayout.ts
@@ -1,28 +1,65 @@
-import { WheelEvent } from "react";
+import { useState, WheelEvent } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { mainRoutes } from "routes/mainRoutes";
+
+type ChangePageStatType = {
+  isLoading: boolean;
+  page: number;
+};
 
 export const useMainLayout = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation() || { pathname: "/" };
   const pathArr: string[] = mainRoutes.map((item) => item.path || "");
-  const currentIndexPage = pathArr.indexOf(pathname);
+  const [currentIndexPage, setCurrentIndexPage] = useState<number>(
+    pathArr.indexOf(pathname),
+  );
+  const [isChangePage, setChangePage] = useState<ChangePageStatType>({
+    isLoading: false,
+    page: currentIndexPage,
+  });
+
+  const handlePush = (route: string, indexPage: number) => {
+    setChangePage({ isLoading: true, page: indexPage });
+
+    //todo: переделать очищение setTimeout
+    //Очищаем сеттаймауты, чтобы коректно перейти на последнюю страницу (не проходя все)
+    for (let i = 1; i < 1000; i++) {
+      clearTimeout(i);
+    }
+
+    setTimeout(() => {
+      navigate(route);
+
+      setChangePage((prevState) => ({
+        ...prevState,
+        isLoading: false,
+      }));
+    }, 3000);
+  };
+
+  const handleChangePage = (newPageIndex: number) => {
+    if (
+      currentIndexPage + newPageIndex >= 0 &&
+      currentIndexPage + newPageIndex <= pathArr.length - 1
+    ) {
+      setCurrentIndexPage((prevState) => prevState + newPageIndex);
+    }
+
+    const path = pathArr[currentIndexPage];
+    handlePush(path, currentIndexPage);
+  };
 
   const handleWhell = (e: WheelEvent<HTMLDivElement>) => {
     const scrollValue = e.deltaY;
     if (scrollValue > 0) {
-      const nextPath = pathArr[currentIndexPage + 1];
-      if (nextPath) {
-        navigate(nextPath);
-      }
+      handleChangePage(1);
     } else {
-      const prevPath = pathArr[currentIndexPage - 1];
-      if (prevPath) {
-        navigate(prevPath);
-      }
+      handleChangePage(-1);
     }
   };
   return {
+    isChangePage,
     handleWhell,
   };
 };
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const IndexPage = () => {
   return (
-    <MainLayout headerTitle="ГЛАВНАЯ">
+    <MainLayout>
       <Index />
     </MainLayout>
   );
