d0b9965d49b09536d47b6bed9b3592bdae1dd5ce
DeEKol <dekolodkin@gmail.com>
2022-11-29 17:27:29 +0500

    add: send form

---
+++ src/components/Contacts/FormBlock/FormElem/style.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+const FormElemSC = styled("li")`
+  border-bottom: 2px solid #ffffff;
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+const FormElemErrorTextSC = styled("p")`
+  position: absolute;
+  color: #c94f4f;
+  font-size: 13px;
+  text-indent: 20px;
+  margin-top: 4px;
+`;
+
+const FormInputSC = styled("input")`
+  color: white;
+  text-indent: 20px;
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  margin: 7px 0;
+  &::placeholder {
+    color: #bbbbbb;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    text-indent: 20px;
+  }
+  &:hover {
+    cursor: none;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const useFormElemStyles = () => ({
+  FormElemSC,
+  FormElemErrorTextSC,
+  FormInputSC,
+});
