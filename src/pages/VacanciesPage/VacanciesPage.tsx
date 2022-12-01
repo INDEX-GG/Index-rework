e9b2c174b1628d16c574f07e093fc58904309d13
DeEKol <dekolodkin@gmail.com>
2022-12-01 15:11:23 +0500

    add: vacancies page

---
+++ src/pages/VacanciesPage/VacanciesPage.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "layout/MainLayout";
+import React from "react";
+
+const VacanciesPage = () => {
+  return (
+    <MainLayout>
+      <div>vacancies</div>
+    </MainLayout>
+  );
+};
+
+export default VacanciesPage;
--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -3,15 +3,26 @@ import ContactsPage from "pages/ContactsPage/ContactsPage";
 import ErrorPage from "pages/ErrorPage/ErrorPage";
 import IndexPage from "pages/IndexPage/IndexPage";
 import PartnersPage from "pages/PartnersPage/PartnersPage";
+import VacanciesPage from "pages/VacanciesPage/VacanciesPage";
 import { RouteObject } from "react-router-dom";
 
-export const pageNameRU = ["Главная", "О нас", "Контакты", "Партнеры"];
+export const pageNameRU = [
+  "Главная",
+  "Вакансии",
+  "О нас",
+  "Контакты",
+  "Партнеры",
+];
 
 export const mainRoutes: RouteObject[] = [
   {
     path: "/",
     element: <IndexPage />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/vacancies",
+    element: <VacanciesPage />,
   },
   {
     path: "/about",
