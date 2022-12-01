6ff8c5053d173847e7cf0f9a0497a8a56158fd4a
DeEKol <dekolodkin@gmail.com>
2022-12-01 15:21:58 +0500

    add: reviews page

---
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "layout/MainLayout";
+import React from "react";
+
+const ReviewsPage = () => {
+  return (
+    <MainLayout>
+      <div>reviews</div>
+    </MainLayout>
+  );
+};
+
+export default ReviewsPage;
--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -4,6 +4,7 @@ import ContactsPage from "pages/ContactsPage/ContactsPage";
 import ErrorPage from "pages/ErrorPage/ErrorPage";
 import IndexPage from "pages/IndexPage/IndexPage";
 import PartnersPage from "pages/PartnersPage/PartnersPage";
+import ReviewsPage from "pages/ReviewsPage/ReviewsPage";
 import VacanciesPage from "pages/VacanciesPage/VacanciesPage";
 import { RouteObject } from "react-router-dom";
 
@@ -14,6 +15,7 @@ export const pageNameRU = [
   "Контакты",
   "Партнеры",
   "Кейсы",
+  "Отзывы",
 ];
 
 export const mainRoutes: RouteObject[] = [
@@ -41,5 +43,9 @@ export const mainRoutes: RouteObject[] = [
   {
     path: "/cases",
     element: <CasesPage />,
+  },
+  {
+    path: "/reviews",
+    element: <ReviewsPage />,
   },
 ];
